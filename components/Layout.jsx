1cf6a9d2fd70de8bdbb93cc2fe88d20f5494d3fd
Suparth Ghimire <suparthnarayanghimire2014@gmail.com>
2022-03-31 13:23:21 +0545

    Head -> head (Solve next/Head not resolved Error)

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,14 @@
 import { ToastContainer } from "react-toastify";
 import HelpModal from "../components/HelpModal";
 import Banner from "./Banner";
+import Head from "next/head";
 export default function Layout({ children }) {
   return (
     <>
-      <Banner />
+      <Head>
+        <title>Talk - E2E Chat Application</title>
+      </Head>
+      {/* <Banner /> */}
       <HelpModal />
       {children}
       <ToastContainer />
